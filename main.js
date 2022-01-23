aeac58eeaff28b30bb2684d0fe5392ad23a243f3
Anniek <anniekgithub@gmail.com>
2022-01-23 18:31:34 +0100

    changed layout and added dropdown-menu

--- main.js
+++ main.js
@@ -5,3 +5,14 @@ function changeColor() {
     --dark-color: ${rando()};
     --light-color: ${rando()};`
   }
+
+
+   /* Toggle Animations */
+   const jstoggle = document.getElementById('js-toggle-stars');
+   jstoggle.addEventListener('click', () => {
+     const animations = document.querySelectorAll('.star');
+     animations.forEach(animation => {
+       const running = animation.style.animationPlayState || 'running';
+       animation.style.animationPlayState = running === 'running' ? 'paused' : 'running';
+     })
+   });
