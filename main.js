2ea5adf6c54a3b59b8f82f566e3c28dd7ea3ac70
Anniek <anniekgithub@gmail.com>
2022-01-28 22:21:33 +0100

    worked on project drums

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
-function changeColor() {
-    const colors = [ 'pink', 'orange' , 'red', 'purple', 'blue', 'teal']
-    const rando = () => colors[Math.floor (Math.random() * colors.length)];
-    document.getElementById("changeColor").style.cssText = `
-    --dark-color: ${rando()};
-    --light-color: ${rando()};`
-  }
 
 
-   /* Toggle Animations */
-   const jstoggle = document.getElementById('js-toggle-stars');
-   jstoggle.addEventListener('click', () => {
-     const animations = document.querySelectorAll('.star');
-     animations.forEach(animation => {
-       const running = animation.style.animationPlayState || 'running';
-       animation.style.animationPlayState = running === 'running' ? 'paused' : 'running';
-     })
-   });
+// key triggers
+// 
+
+window.addEventListener('keydown', (e) =>{
+    const audio = document.querySelector(`audio[data-key=${e.key}]`)
+    if(!audio) return;
+    audio.currentTime = 0; //rewind to start
+    
+    audio.play();
+})
+
+// audio[data-key='${e.key}' | code='${e.code}"]`
