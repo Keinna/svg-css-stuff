8a575785f2d22f55a7e9794500401716453705e7
Anniek <anniekgithub@gmail.com>
2022-01-29 15:27:17 +0100

    worked on drumkit

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 
+function changeColor() {
+    const colors = [ 'pink', 'orange' , 'red', 'purple', 'blue', 'teal']
+    const rando = () => colors[Math.floor (Math.random() * colors.length)];
+    document.getElementById("changeColor").style.cssText = `
+    --dark-color: ${rando()};
+    --light-color: ${rando()};`
+  }
 
-// key triggers
-// 
+
+   /* Toggle Animations */
+   const jstoggle = document.getElementById('js-toggle-stars');
+   jstoggle.addEventListener('click', () => {
+     const animations = document.querySelectorAll('.star');
+     animations.forEach(animation => {
+       const running = animation.style.animationPlayState || 'running';
+       animation.style.animationPlayState = running === 'running' ? 'paused' : 'running';
+     })
+   });
+
+// key triggers drums
 
 window.addEventListener('keydown', (e) =>{
     const audio = document.querySelector(`audio[data-key=${e.key}]`)
@@ -11,4 +28,5 @@ window.addEventListener('keydown', (e) =>{
     audio.play();
 })
 
-// audio[data-key='${e.key}' | code='${e.code}"]`
+
+
